ae4eb9b1e6a12190e14270a5a7aa94d64e97354c
sreeshankreddy <23eg106b40@anurag.edu.in>
2025-08-22 12:30:11 +0530

    Update program with latest changes

---
+++ app_server/controllers/index.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const router = express.Router();
+const ctrlLocations = require('../controllers/locations');
+const ctrlOthers = require('../controllers/others');
